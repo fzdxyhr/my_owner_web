33af8fee4adfec7920bb63bc0f57010c0d863ed4
yanhr <yanhr@101.com>
2016-09-06 16:18:50 +0800

    创建实体类

---
+++ my-web/src/main/java/com/yhr/vo/UserVo.java
@@ -0,0 +1,17 @@
+package com.yhr.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * Created by YHR on 2016/8/13.
+ */
+@Data
+public class UserVo {
+    @ApiModelProperty("用户id")
+    private Long id;
+    @ApiModelProperty("用户名")
+    private String UserName;
+    @ApiModelProperty("用户密码")
+    private String password;
+}
---
+++ my-web/src/test/java/UserControllerTest.java
@@ -0,0 +1,8 @@
+/**
+ * @author yhr
+ * @version latest
+ * @date 2016/9/6
+ * @description
+ */
+public class UserControllerTest {
+}
